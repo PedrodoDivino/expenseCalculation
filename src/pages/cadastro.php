4e0aa9561768cd32044f752e3e114e6b9293da15
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-10-15 08:42:46 -0300

    commit inicial do projeto

---
+++ src/pages/cadastro.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Cadastro</title>
+</head>
+<body>
+    <div class="main-login">
+        <div class="left-login">
+            <h1>Crie sua Conta na<br>Expense Calculation!</h1>
+            <img src="informacoes.svg" class="left-login-imagem" alt="Financas animacao">
+        </div>
+        <div class="right-login">
+        <div class="tela-login">
+            <div class="textfield">
+                <label for="nome">Nome</label>
+                <input type="text" name="nome" placeholder="Nome">
+            </div>   
+            <div class="textfield">
+                   <label for="usuario">E-mail</label>
+                    <input type="text" name="usuario" placeholder="E-mail">
+                </div>
+                <div class="textfield">
+                   <label for="senha">Senha</label>
+                    <input type="password" name="senha" placeholder="Senha">
+                </div>
+                <div class="textfield">
+                   <label for="confirme-senha">Confirme sua Senha</label>
+                    <input type="password" name="senha" placeholder="Confirme-senha">
+                </div>
+                <div class="tela-cadastro">
+                <p>Ao cadastrar-me declaro que estou ciente e <br>aceito os termos de<a href="https://cieb.net.br/lgpd/?gclid=CjwKCAjw7p6aBhBiEiwA83fGuv2JaG_Dh8A7-BzM6b7SG_vNh5aefOV5yKBkIOcLUlWvYHzDQpuqaBoCn3EQAvD_BwE" target="_blank" > Politica de Privacidade.</a></p>
+                </div>
+                <button class="botao-login">CRIAR</button>
+            </div>
+        </div>
+    </div>
+   
+</body>
+</html>
---
+++ src/pages/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="main-login">
+        <div class="left-login">
+            <h1>Seja Bem vindo(a)<br>Faça seu Login</h1>
+            <img src="financas.svg" class="left-login-imagem" alt="Financas animacao">
+        </div>
+        <div class="right-login">
+        <div class="tela-login">
+                <h1>LOGIN</h1>   
+            <div class="textfield">
+                   <label for="usuario">Usuário</label>
+                    <input type="text" name="usuario" placeholder="E-mail">
+                </div>
+                <div class="textfield">
+                   <label for="senha">Senha</label>
+                    <input type="password" name="senha" placeholder="Senha">
+                </div>
+                <div class="tela-cadastro">
+                <p id="conta">Não tem uma conta? <a href="cadastro.php" target="_blank" > Crie uma Conta.</a></p>
+                </div>
+                <a href="materia-prima.svg"><button class="botao-login">Entrar</button></a>
+            </div>
+        </div>
+    </div>
+   
+</body>
+</html>
