29e941c08ff8fed6840a04853e52ba954d3d68e5
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-10-26 20:28:39 -0300

    main

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # expense_calculation
 Esse projeto é parte de um trabalho com de php que estamos desenvolvendo em grupo na univer
 
-#  Depois que você fez download do projeto, precisa só entrar em index.php
-ele está assim, sem css e tudo, pois eu mudei a organização de
-documentos da pagina, mas vou ajustar tudo corretamente agora ok
+# Utilizamos o bootstrap  para o nosso estilo
+**https://getbootstrap.com/docs/5.2**
--- index.php
+++ index.php
@@ -1,35 +1,28 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./src/styles/login.css"> 
-    <title>Login</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="style.css">
+    <title>Document</title>
 </head>
+
 <body>
-    <div class="main-login">
-        <div class="left-login">
-            <h1>Seja Bem vindo(a)<br>Faça seu Login</h1>
-            <img src="financas.svg" class="left-login-imagem" alt="Financas animacao">
-        </div>
-        <div class="right-login">
-        <div class="tela-login">
-                <h1>LOGIN</h1>   
-            <div class="textfield">
-                   <label for="usuario">Usuário</label>
-                    <input type="text" name="usuario" placeholder="E-mail">
-                </div>
-                <div class="textfield">
-                   <label for="senha">Senha</label>
-                    <input type="password" name="senha" placeholder="Senha">
-                </div>
-                <div class="tela-cadastro">
-                <p id="conta">Não tem uma conta? <a href="cadastro.php" target="_blank" > Crie uma Conta.</a></p>
-                </div>
-                <a href="materia-prima.php"><button class="botao-login">Entrar</button></a>
-            </div>
+    <div class="container text-center">
+  <div class="row">
+    <div class="col">
+        <div class="container" >
+            <h1>Seja bem vindo(a)</h1>
+            <h1>Faça o seu login</h1>
         </div>
     </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
 </body>
-</html>
+</html> 
