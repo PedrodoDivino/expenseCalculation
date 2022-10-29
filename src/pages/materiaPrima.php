d3a19e89aed499d643e364f5ea988d5c022a97c2
bavelar21 <bruno.avelar04@gmail.com>
2022-10-29 11:54:19 -0300

    Tela matéria prima

---
+++ src/pages/materiaPrima.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styles/estoque.css">
+    <title>MATÉRIA PRIMA</title>
+</head>
+<body>
+    
+    <header>
+        <img class="logo" src="logoExpense.png" alt="logo">
+
+        <nav>
+            <ul class="nav_links">
+ 
+                <li><a href="#">MATÉRIA PRIMA</a></li>
+                <li><a href="#">ENTRADA</a></li>
+                <li><a href="#">MINHAS RECEITAS</a></li>
+                <li><a href="#">CADASTRO DE PRODUTOS</a></li>
+                <li><a href="#">NOVAS RECEITAS</a></li>
+                <li><a href="#">MEUS RESULTADOS</a></li>
+ 
+            </ul>
+        </nav>
+    </header>
+
+    <h1>ESTOQUE DE PRODUTOS</h1>
+    <table>
+        <tr>
+            <td>Produto</td>
+            <td>Quantidade(gr)</td>
+            <td>Valor(R$)</td>
+        </tr>
+        <tr>
+            <td>Produto</td>
+            <td>Quantidade(gr)</td>
+            <td>Valor(R$)</td>
+        </tr>
+    </table>
+    
+</body>
+</html>
