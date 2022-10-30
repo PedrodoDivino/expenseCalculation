285f8aaee5d22277adc133c8475eb964293c1bf5
igorenriq <igor_enriq@hotmail.com>
2022-10-29 22:15:35 -0300

    add tela meusResultados.php

---
+++ src/pages/meusResultados.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../styles/meusResultados.css">
+</head>
+<body>
+    <header>
+        <img class="logo" src="https://raw.githubusercontent.com/PedrodoDivino/expenseCalculation/main/src/pages/logoExpense.png"logo">
+        <nav>
+            <ul class="nav_links">
+ 
+                <li><a href="#">MATÉRIA PRIMA</a></li>
+                <li><a href="#">ENTRADA</a></li>
+                <li><a href="#">MINHAS RECEITAS</a></li>
+                <li><a href="#">CADASTRO DE PRODUTOS</a></li>
+                <li><a href="#">NOVAS RECEITAS</a></li>
+                <li><a href="#">MEUS RESULTADOS</a></li>
+ 
+            </ul>
+        </nav>
+    </header>
+      <center>
+        <h3>Resumo De Despesas e Lucros</h3>
+      </center>
+      <h5>Não Calculamos seu Gás</h5>
+      <h5>por restrições de cada</h5>
+     <h5> forno ou fogão utilizados é</h5>
+     <h5> necessario conferir no </h5>
+     <h5> manual do seu fogão.</h5><br>
+             <h5>Gasto em R$ = (((T/60)*Kg/</h5>
+             <h5>h)/B)*PB</h5><
+                 <h5>Onde:</h5>
+                 <h5>T = Tempo de uso em</h5>
+                 <h5> minutos dividido por 60;</h5>
+                 <h5>Kg/h = Consumo por</h5> 
+                 <h5>queimador, em Kg/h (ver o</h5>
+                 <h5> manual do fabricante do</h5>
+                 <h5> fogão);</h5>
+                 <h5>B = Capacidade do botijão </h5>
+                 <h5>(13 ou 45 Kg);</h5>
+                 <h5>PB = Preço do Botijão.</h5>
+         <form>    
+            <center> 
+                <div class="button">
+                    <button type="submit" class="btn btn-primary">Minhas Receitas</button>
+                      </div>
+                  </div>
+                </center>
+                </form>  
+ 
+           
+</body>
+</html>
