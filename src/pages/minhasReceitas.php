b370e3eb10c8809dac4b08049337df13254eba77
ReCanelossi <renatocanelossi28@hotmail.com>
2022-10-31 23:48:00 -0300

    minhas receitas

---
+++ src/pages/minhasReceitas.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+     <link rel="stylesheet" href="../styles/minhasReceitas.css">
+    <title>minhas receitas</title>
+</head>
+<header>
+        <img class="logo" src="logoExpense.png" alt="logo">
+
+        <nav>
+            <ul class="nav_links">
+                  <li><a href="materiaPrima.php">MATÉRIA PRIMA</a></li>
+                <li><a href="entradas.php">ENTRADA</a></li>
+                <li><a href="minhasReceitas.php">MINHAS RECEITAS</a></li>
+                <li><a href="cadastroProdutos.php">CADASTRO DE PRODUTOS</a></li>
+                <li><a href="novasReceitas.php">NOVAS RECEITAS</a></li>
+                <li><a href="resultados.php">MEUS RESULTADOS</a></li>
+            </ul>
+        </nav>
+    </header>
+<body>
+
+    
+    <div class="card" style="width: 18rem;">
+  <div class=" fundoCard card-body">
+    <h1 class="card-title">bolo de cenura</h1>
+    <p class="card-text"><h2> massa</h2> 
+    <p>1/2 xícara (chá) de óleo 
+    <br>  
+3 cenouras médias raladas
+<br>
+4 ovos
+<br>
+2 xícaras (chá) de açúcar
+<br>
+2 e 1/2 xícaras (chá) de farinha de trigo
+<br>
+1 colher (sopa) de fermento em pó
+<br>
+<h2>COBERTURA</h2>
+  <p>
+1 colher (sopa) de manteiga
+<br>
+3 colheres (sopa) de chocolate em pó
+<br>
+1 xícara (chá) de açúcar
+<br>
+1 xícara (chá) de leite
+</p>   
+<a href="#" class="btn btn-primary">fabricar</a>
+  </div>
+</div>
+</body>
+</html>
