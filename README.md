3949f019a9e56ca4f6c80dc4c3d845b22807806a
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-11-02 11:33:37 -0300

    [fix] ajustes

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 Esse projeto é parte de um trabalho com de php que estamos desenvolvendo em grupo na univer
 
 # Utilizamos o bootstrap  para o nosso estilo
-**https://getbootstrap.com/docs/5.2**
+**https://getbootstrap.com/docs/5.2** 
+
+
+# Link para a pagina index:
+http://localhost/expenseCalculation/src/pages/
--- src/pages/cadastroProdutos.php
+++ src/pages/cadastroProdutos.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../styles/cadastroProdutos.css">
-    <title>MATÉRIA PRIMA</title>
+    <title>Cadastro Produtos</title>
     <link rel="stylesheet" href="style.css">
 
 </head>
@@ -23,7 +23,7 @@
                 <li><a href="minhasReceitas.php">MINHAS RECEITAS</a></li>
                 <li><a href="cadastroProdutos.php">CADASTRO DE PRODUTOS</a></li>
                 <li><a href="novasReceitas.php">NOVAS RECEITAS</a></li>
-                <li><a href="resultados.php">MEUS RESULTADOS</a></li>
+                <li><a href="meusResultados.php">MEUS RESULTADOS</a></li>
             </ul>
         </nav>
     </header>
@@ -36,17 +36,9 @@
           <h5>Descrição</h5>
           <input type="text" class="form-control"  placeholder="Ex: Rico em vitamina A">
         </div>   
-       
-        <div class="containerImage">
-             <img class="logo" src="https://raw.githubusercontent.com/PedrodoDivino/expenseCalculation/main/src/pages/logoExpense.png" alt="logo">    
-        </div>
-        
        <div class="button">
 <button type="submit" class="btn btn-primary">Salvar</button>
 </div>
-
-
-
-  </div>
+</div>
 </body>
 </html>
