516cc19dbd04f7be37cc7a3a98867837c701a33d
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-11-09 00:26:21 -0300

    tela novas receitas com modal

--- src/pages/cadastroMateriaPrima.php
+++ src/pages/cadastroMateriaPrima.php
@@ -49,26 +49,3 @@
 </div>
 </body>
 </html>
-
-
-<!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-Salvar
-</button>
-
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Titulo</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-    Meu conteúdo aqui
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-        <button type="button" class="btn btn-primary">Salvar</button>
-      </div>
-    </div>
-  </div>
-</div> -->
--- src/pages/novasReceitas.php
+++ src/pages/novasReceitas.php
@@ -21,23 +21,34 @@
   </nav>
 </header>
 <body>
+  <form method="post">
     <div class="container">
 <div class="row">
   <div class="col">
     Nome da Receita
-    <input type="text" class="form-control"  aria-label="First name" required>
+    <input type="text" class="form-control" name="nomeReceita" aria-label="First name" required>
   </div>
   <div class="col">
      Margem de lucro %
-    <input type="text" class="form-control"  aria-label="Last name" minlength="1" maxlength="100">
+    <input type="number" class="form-control"   name="margemLucro" aria-label="Last name" minlength="1" maxlength="100">
   </div>
-  <div class="row">
-  <div class="col">
-    <div style="heigth"></div>
-    Ingredientes
+<div class="col">
+    Embalagem
+    <input type="text" class="form-control" name="embalagem" aria-label="Last name" minlength="1" maxlength="100">
+</div>
+<div class="col">
+    Data Produção
+    <input type="date" class="form-control" name="dataProducao" aria-label="Last name" minlength="1" maxlength="100">
+</div>
+  <div class="ingredientes">
+    <form  method="post">
+      <div class="row">
+          <div class="col">
+            <div style="heigth"></div>
+               Ingredientes
   <div class="space"></div>
    <div class="space form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <input class="form-check-input" type="checkbox" name="nomeItem" id="gridCheck">
       <label class="form-check-label" for="gridCheck">
         Farinha
       </label>
@@ -105,12 +116,55 @@
   </div>
 </div>
 </div> 
+</form>
 <br>
+  </div>
   </div>
   <div class="info"><p>Obs: Ovo Médio Equivale 53gr!</p></div>
   <div class="button">
-<button type="submit" class="btn btn-primary">Salvar</button>
+<button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+Continuar
+</button>
+</form>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Titulo</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+<form action="minhasReceitas.php" method="post">
+    <div class="col">
+    Nome do produto
+    <input type="text" class="form-control"  aria-label="First name" required>
+  </div>
+      <div class="col">
+   Preço final
+    <input type="number" class="form-control"  aria-label="First name" required>
+  </div>
+      <div class="col">
+   Tipo do Produto
+    <input type="text" class="form-control"  aria-label="First name" required>
+  </div>
+      <div class="col">
+    Descrição do produto
+    <input type="text" class="form-control"  aria-label="First name" required>
+  </div>
+      <div class="col">
+    Data de vencimento
+    <input type="date" class="form-control"  aria-label="First name" required>
+  </div>
 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+        <button type="submit" class="btn btn-primary">Salvar</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
   </div>
    
 </body>
