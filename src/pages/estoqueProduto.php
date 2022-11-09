99b4d3078f95a75c2c319a68cbcb957c5a88d617
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-11-09 00:34:15 -0300

    ajustes tabela

--- src/pages/estoqueProduto.php
+++ src/pages/estoqueProduto.php
@@ -24,50 +24,56 @@
 
     <div class="container">
     <table class="table">
-  <caption>Lista de produtos</caption>
   <thead>
   <tr class="table-dark">
-      <th scope="col">#</th>
-      <th scope="col">Produto</th>
-      <th scope="col">Quantidade(gr)</th>
-      <th scope="col">Valor(R$)</th>
+      <th scope="col">Nome Materia Prima</th>
+      <th scope="col">Quantidade(gr)</th>  
+      <th scope="col">Data Entrada</th>   
+     <th scope="col">Descrição</th>
+      <th scope="col">Valor Total R$</th>
     </tr>
   </thead>
   <tbody>
     <tr class="table-light">
-      <th scope="row">1</th>
       <td>Chocolate</td>
         <td>700</td>
+        <td>20/11/3212</td>
+         <td>Cacau</td>
         <td>20,00</td>
     </tr>
     <tr class="table-light">
-      <th scope="row">2</th>
         <td>Farinha</td>
         <td>1000</td>
+          <td>20/11/3212</td>
+         <td>Cacau</td>
         <td>62,32</td>
     </tr>
     <tr class="table-light">
-      <th scope="row">3</th>
         <td>Ovo</td>
         <td>30</td>
+          <td>20/11/3212</td>
+         <td>Cacau</td>
         <td>45,75</td>
     </tr>
     <tr class="table-light">
-    <th scope="row">4</th>
         <td>Açúcar</td>
         <td>800</td>
+          <td>20/11/3212</td>
+         <td>Cacau</td>
         <td>30,00</td>
     </tr>
     <tr class="table-light">
-    <th scope="row">5</th>
         <td>Óleo</td>
         <td>1200</td>
+          <td>20/11/3212</td>
+         <td>Cacau</td>
         <td>95,23</td>
     </tr>
     <tr class="table-light">
-    <th scope="row">6</th>
         <td>Leite</td>
         <td>1000</td>
+          <td>20/11/3212</td>
+         <td>Cacau</td>
         <td>80,00</td>
     </tr>
   </tbody>
