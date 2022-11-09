2d0e3db49091f1aad79088536487a425a827bc11
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-11-09 00:39:07 -0300

    Tela estoque

--- src/pages/minhasReceitas.php
+++ src/pages/minhasReceitas.php
@@ -42,7 +42,25 @@ Açúcar 200g
 <br>
 Leite 350gr
 </p>   
-<a href="#" class="btn btn-primary">Fabricar</a>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+ Fabricar
+</button>
+  </div>
+</div>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Titulo</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       meu resumo aqui
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Voltar</button>
+      </div>
+    </div>
   </div>
 </div>
 </body>
