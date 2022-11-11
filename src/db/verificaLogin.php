3920eca5c616388c32701fa157a6749a1c24bbf4
LeoOliveiraf <oliveiraleo797@gmail.com>
2022-11-11 10:44:56 -0300

    [FIX] 'Inicio ajuste Php'

--- src/db/verificaLogin.php
+++ src/db/verificaLogin.php
@@ -1,11 +1,11 @@
 <?php 
-    include '../db/conectabanco.php';
-    $usuario = $_POST ['usuario'];
-    $senha = $_POST ['senha'];
-    $sql = "SELECT * FROM USER WHERE email = '$usuario' AND senha = '$senha'";
-    $conect=mysqli_query($banco, $sql);  
-    echo $usuario, $senha;
-    
+
+function verification($path){ 
+
+    if(!is_string($_SESSION['email'])){
+        header('location: '.$path);
+        exit;
+    }
+}
 
 ?>
-
