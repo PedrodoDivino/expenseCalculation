8be10cdecf9dbb070f781ebf39f7f2ab99a3f142
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-11-19 14:50:46 -0300

    [Pedro] Enviando dados para o banco

--- src/routes/novasreceitas.php
+++ src/routes/novasreceitas.php
@@ -5,11 +5,12 @@ include('../db/conectaBanco.php');
     $nome = $_POST['nome']; 
     $embalagem = $_POST['embalagem']; 
     $data_producao= $_POST['data_producao'];
-    
+
     $insertNovasReceitas = "INSERT INTO receitas ( margem_lucro, nome, embalagem, data_producao)
-    VALUES ('$margem_lucro', $nome', '$embalagem', '$data_producao')";
+    VALUES ('$margem_lucro', '$nome', '$embalagem', '$data_producao')";
 
     $mysqli->query($insertNovasReceitas);
+    header("location: http://localhost/expenseCalculation/src/pages/novasReceitas.php");
 
 ?>
 
